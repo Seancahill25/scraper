2158579594bf25be7831ad3d7628f769df42dc42
Sean Cahill <seancahill25@gmail.com>
2018-07-07 16:05:31 +0000

    fulling pulling data from portfolio

--- program.cs
+++ program.cs
@@ -10,33 +10,40 @@ class Scraper
     static void Main(string[] args)
     {
         // Create a new instance of the Chrome driver.
-            var options = new ChromeOptions();
-            options.AddArguments("--disable-gpu");
-            var driver = new ChromeDriver();
-        
-            driver.Navigate().GoToUrl("https://login.yahoo.com");
+        var options = new ChromeOptions();
+        options.AddArgument("--disable-gpu");
+        options.AddArgument("--ignore-certificate-errors");
+        options.AddArgument("--ignore-ssl-errors");
+        var driver = new ChromeDriver();
 
-            var username =  driver.FindElement(By.Name("username"));
-            username.SendKeys("seanscrap25");
-            username.Submit();
 
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
-            wait.Until(d => d.FindElement(By.Id("login-passwd")));
+        driver.Navigate().GoToUrl("https://login.yahoo.com");
 
-            var password = driver.FindElement(By.Id("login-passwd"));
-            password.SendKeys("Potatosalad33");
-            password.SendKeys(Keys.Enter);
+        var username = driver.FindElement(By.Name("username"));
+        username.SendKeys("seanscrap25");
+        username.Submit();
 
-            driver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_1/view/v1");
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+        wait.Until(d => d.FindElement(By.Id("login-passwd")));
+
+        var password = driver.FindElement(By.Id("login-passwd"));
+        password.SendKeys("Potatosalad33");
+        password.SendKeys(Keys.Enter);
+
+        driver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_1/view/v1");
 
         wait.Until(d => d.FindElement(By.Id("__dialog")));
         var popup = driver.FindElement(By.XPath("//dialog[@id = '__dialog']/section/button"));
-            popup.Click();
+        popup.Click();
 
-            var Stock1Price = driver.FindElement(By.XPath("//*[@id='main']/section/section[2]/div[2]/table/tbody/tr[1]/td[1]/span/a/title"));
-            Console.WriteLine(Stock1Price);
+        var table = driver.FindElement(By.CssSelector("#main > section > section._64nqq > div.gIc8M > table"));
+        foreach (var row in table.FindElements(By.TagName("tr")))
+        {
+                foreach (var cell in row.FindElements(By.TagName("td")))
+                {
+                Console.WriteLine(cell.Text);
+                }
 
-        //List<IWebElement> elements = new List<IWebElement>();
-        //elements = driver.FindElements(By.XPath("//tbody/tr")).ToList<IWebElement>();
+        }
     }
 }
